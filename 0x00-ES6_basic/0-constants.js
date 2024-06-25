4d32d4ce5e7580e20407c29f442210ee9bf5cc76
AbuArwa001 <khalfanathman12@yahoo.com>
2024-06-25 16:17:13 +0300

    Add eslint compliance

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,6 +1,5 @@
 //  0-constants.js
-export const taskFirst = function taskFirst() {
-  const task = 'I prefer const when I can.';
+export const taskFirst = function taskFirst(task = 'I prefer const when I can.') {
   return task;
 };
 
@@ -9,9 +8,7 @@ export function getLast() {
 }
 
 /* eslint-disable import/no-mutable-exports, prefer-const */
-export let taskNext = function taskNext() {
-  let combination = 'But sometimes let';
-  combination += getLast();
-  return combination;
+export let taskNext = function taskNext(combination = 'But sometimes let') {
+  return combination + getLast();
 };
 /* eslint-enable import/no-mutable-exports, prefer-const */
