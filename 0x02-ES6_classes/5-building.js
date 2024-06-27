32f244dcfb689b6b0435a33af0c8a5f34a36979a
AbuArwa001 <khalfanathman12@yahoo.com>
2024-06-28 00:43:45 +0300

    Refactor building module

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,23 +1,21 @@
 class Building {
   constructor(sqft) {
+    if (new.target === Building) {
+      throw new Error('Cannot instantiate abstract class Building directly');
+    }
+
     if (typeof sqft !== 'number') {
       throw new TypeError('sqft must be a number');
     }
-    if (this.constructor !== Building && typeof this.evacuationWarningMesagge !== 'function') {
+    this._sqft = sqft;
+
+    if (this.evacuationWarningMessage === undefined) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-    this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new TypeError('Amount must be a number');
-    }
-    this._sqft = sqft;
   }
 }
 
