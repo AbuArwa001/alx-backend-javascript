d3093c15454e3ed6b77b1061f44f487bec690b4e
AbuArwa001 <khalfanathman12@yahoo.com>
2024-06-27 04:32:17 +0300

    Await promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,19 @@
+import { uploadPhoto, createUser } from './utils.js';
+
+async function handleProfileSignup() {
+  try {
+    // Assuming these functions return promises
+    await uploadPhoto();
+    const userResult = await createUser();
+
+    // Extract firstName and lastName from the results
+    const { firstName, lastName } = userResult.body;
+
+    // Log the values to the console
+    console.log(`User profile created: ${firstName} ${lastName}`);
+  } catch (error) {
+    console.error('Signup system offline');
+  }
+}
+
+handleProfileSignup();
