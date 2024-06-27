2a81af46cf91b42b9dd45e66089c58d7bbe16f2a
AbuArwa001 <khalfanathman12@yahoo.com>
2024-06-27 23:33:52 +0300

    AImplement abstract clas

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,24 @@
+class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    if (this.constructor !== Building && typeof this.evacuationWarningMesagge !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._sqft = sqft;
+  }
+}
+
+export default Building;
