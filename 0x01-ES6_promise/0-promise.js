a2737af17f4900b1d6f4e6de8e8fb836c96f544a
AbuArwa001 <khalfanathman12@yahoo.com>
2024-06-27 03:15:01 +0300

    Get response from api

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,12 +1,12 @@
-const getResponseFromAPI = () => {
-    return new Promise((resolve, reject) => {
-      // Simulating an asynchronous operation
-      setTimeout(() => {
-        // For the sake of example, resolving the promise
-        resolve("Success");
-      }, 1000); // Delay of 1 second
-    });
-  };
-  
-  export default getResponseFromAPI;
-  
+const getResponseFromAPI = () => new Promise((resolve, reject) => {
+  try {
+    // Simulating an asynchronous operation
+    setTimeout(() => {
+      resolve('Success');
+    }, 1000); // Delay in milliseconds
+  } catch (error) {
+    reject(error);
+  }
+});
+
+export default getResponseFromAPI;
