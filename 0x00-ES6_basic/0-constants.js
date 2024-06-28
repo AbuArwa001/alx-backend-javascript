3f613d46b8a677aa02914b6f9c6b682129949c84
AbuArwa001 <khalfanathman12@yahoo.com>
2024-06-28 08:01:14 +0300

    Use consta and let

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,14 +1,15 @@
-//  0-constants.js
-export const taskFirst = function taskFirst(task = 'I prefer const when I can.') {
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
   return task;
-};
+}
 
 export function getLast() {
   return ' is okay';
 }
 
-/* eslint-disable import/no-mutable-exports, prefer-const */
-export let taskNext = function taskNext(combination = 'But sometimes let') {
-  return combination + getLast();
-};
-/* eslint-enable import/no-mutable-exports, prefer-const */
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
