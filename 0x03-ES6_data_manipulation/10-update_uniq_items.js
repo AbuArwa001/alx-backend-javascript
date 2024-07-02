e8de152b6abf7d008968ec1c1b0f9bf4b7a1da37
AbuArwa001 <khalfanathman12@yahoo.com>
2024-07-02 11:13:47 +0300

    Update unique items

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,3 +1,14 @@
+/**
+  * updates Unique Items.
+  * @author Khalfan Athman <https://github.com/AbuArwa001>
+  * @returns {Map(5) {
+  *  'Apples' => 10,
+  * 'Tomatoes' => 10,
+  * 'Pasta' => 100,
+  * 'Rice' => 100,
+  * 'Banana' => 5
+  * }}
+ */
 export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
     return Error('Cannot process');
