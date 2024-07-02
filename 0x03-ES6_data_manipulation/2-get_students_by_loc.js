547398ff4be3b52ce6038864ed2a806ace823f80
AbuArwa001 <khalfanathman12@yahoo.com>
2024-07-02 10:53:49 +0300

    Add getstudents comment

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,8 @@
+/**
+ * Retrieves a filtered list of student from a particular location.
+ * @author Khalfan Athman <https://github.com/AbuArwa001>
+ * @returns {[1,2,5]}
+ */
 export default function getListStudentIds(students, location) {
   return students.filter((val) => location === val.location);
 }
