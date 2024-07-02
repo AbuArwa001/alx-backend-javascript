598583390ab86980827160c923d0f7927a00b202
AbuArwa001 <khalfanathman12@yahoo.com>
2024-07-02 08:30:17 +0300

    Clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+export default function cleanSet(set, startString) {
+  let conc = '';
+  if (!startString) {
+    return conc;
+  }
+  set.forEach((element) => {
+    let newElement = '';
+    if (element.startsWith(startString)) {
+      newElement = element.slice(startString.length);
+      conc += `${newElement}-`;
+    }
+  });
+  return conc.slice(0, -1);
+}
