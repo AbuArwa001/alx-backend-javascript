4e5a0f2028f48c9cc04a3187e9f585ac9412eeb6
AbuArwa001 <khalfanathman12@yahoo.com>
2024-07-02 11:05:46 +0300

    Check for values in sets

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,3 +1,8 @@
+/**
+ * creates set From an Array.
+ * @author Khalfan Athman <https://github.com/AbuArwa001>
+ * @returns {[]}
+*/
 export default function setFromArray(array) {
   return Array.isArray(array) ? new Set(array) : new Set([]);
 }
--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,3 +1,8 @@
+/**
+ * checks if set has Values From an Array.
+ * @author Khalfan Athman <https://github.com/AbuArwa001>
+ * @returns {true}
+ */
 export default function hasValuesFromArray(set, array) {
   return array.every((element) => set.has(element));
 }
