fc8c1e6341f8b593cb99131262aa4d6296951cc0
AbuArwa001 <khalfanathman12@yahoo.com>
2024-08-20 15:26:01 +0300

    Print with stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,10 @@
+const quiz = 'Welcome to Holberton School, what is your name?\n';
+process.stdout.write(quiz);
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
+  process.exit();
+});
+
+process.on('exit', () => {
+  process.stdout.write('This important software is now closing\n');
+});
