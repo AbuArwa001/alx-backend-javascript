d6a18a9b6933985e71e3589e763a6a175eb0897d
AbuArwa001 <khalfanathman12@yahoo.com>
2024-08-22 17:02:56 +0300

    calculte 1-calculate

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,6 +1,9 @@
+/* eslint-disable jest/prefer-expect-assertions */
+/* eslint-disable jest/expect-expect */
 const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
 
+// eslint-disable-next-line jest/prefer-expect-assertions, jest/expect-expect
 describe('calculateNumber', () => {
   it('should return 4 when inputs are 1 and 3', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
@@ -13,6 +16,7 @@ describe('calculateNumber', () => {
   it('should return 5 when inputs are 1.2 and 3.7', () => {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
+
 
   it('should return 6 when inputs are 1.5 and 3.7', () => {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
